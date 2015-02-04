d15caab5255c649cb15de23db279d86bb8139e51
Claus Nielsen <cln@sekoia.dk>
2015-02-04 22:55:12 +0100

    Adding an empty home page

---
+++ Nancy.Demo.AzureWebSitesWithWebJobs/Modules/HomeModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nancy.Demo.AzureWebSitesWithWebJobs.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+            : base("/")
+        {
+            Get[""] = _ => View["Home"];
+        }
+    }
+}
