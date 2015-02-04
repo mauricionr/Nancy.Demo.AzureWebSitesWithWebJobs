e00082977e3e7415c6bc40743940a92d714bccbe
Claus Nielsen <cln@sekoia.dk>
2015-02-04 22:36:19 +0100

    Adding OWIN startup file

---
+++ Nancy.Demo.AzureWebSitesWithWebJobs/Infrastructure/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Nancy.Demo.AzureWebSitesWithWebJobs.Infrastructure;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace Nancy.Demo.AzureWebSitesWithWebJobs.Infrastructure
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+}
