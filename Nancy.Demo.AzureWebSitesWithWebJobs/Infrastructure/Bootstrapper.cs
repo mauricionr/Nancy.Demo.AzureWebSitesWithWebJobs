f7b3c0166ac7a9aab6f7a7353bed645f1f1f6b76
Claus Nielsen <cln@sekoia.dk>
2015-02-05 00:33:25 +0100

    Moving spinner to local file for https support Adding bootstrapper including requiring https

---
+++ Nancy.Demo.AzureWebSitesWithWebJobs/Infrastructure/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy.Bootstrapper;
+using Nancy.Conventions;
+using Nancy.TinyIoc;
+
+namespace Nancy.Demo.AzureWebSitesWithWebJobs.Infrastructure
+{
+    public class Bootstrapper : Nancy.DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+
+            pipelines.BeforeRequest.AddItemToStartOfPipeline(Nancy.Security.SecurityHooks.RequiresHttps(true));
+        }
+    }
+}
