a6848f73d4a1b687f65b8c685b977da8a8ac4816
Claus Nielsen <cln@sekoia.dk>
2015-02-12 15:24:29 +0100

    Adding Image signalR Hub

---
+++ Nancy.Demo.AzureWebSitesWithWebJobs/Hubs/ImagesHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Nancy.Demo.AzureWebSitesWithWebJobs.Hubs
+{
+    public class ImagesHub : Hub
+    {
+        public void InformClientsOfNewImage()
+        {
+            Clients.All.imageUploaded();
+        }
+    }
+}
